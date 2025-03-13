df9890c6ef0523627c5563bd2855ed97473f6c45
Luis Heitor Santana <103472722+hetoi@users.noreply.github.com>
2025-03-13 15:16:06 -0300

    Rascunho inicial do exercício
    Ainda preciso fazer alguns ajustes para conseguir rodar o programa.
    Está dando os seguintes problemas:
    - Ele não quebrou a linha para aparecer o resultado
    - O programa não conseguiu dizer a jogada de cada um

---
+++ exercicio45.py
@@ -0,0 +1,41 @@
+# Crie um programa que faça o computador jogar Jokenpô com você.
+
+import random
+from time import sleep
+
+print("""
+SUAS OPÇÕES: 
+[ 1 ] PEDRA
+[ 2 ] PAPEL
+[ 3 ] TESOURA
+""")
+
+escolha_jogador = int(input("Qual a sua jogada? "))
+escolha_programa = random.randint(1,3)
+if escolha_jogador <= 3:
+    print("JO")
+    sleep(1)
+    print("KEN")
+    sleep(1)
+    print("PO!!!")
+    
+    if escolha_jogador == escolha_programa :
+        mensagem = "O jogo EMPATOU!"
+    elif escolha_jogador == 1 and escolha_programa == 2:
+        mensagem = "O jogador PERDEU"
+    elif escolha_jogador == 1 and escolha_programa == 3:
+        mensagem = "O jogador GANHOU"
+    elif escolha_jogador == 2 and escolha_programa == 3: 
+        mensagem = "O jogador PERDEU" 
+    
+    # É preciso .format dos prints seguintes 
+    # Quebrar a linha da mensagem 
+    print("COMPUTADOR JOGOU {}".format(mensagem))
+    print("JOGADOR JOGOU {}".format(mensagem))
+
+else:
+    print("OPÇÃO INVÁLIDA!")
+ 
+
+
+

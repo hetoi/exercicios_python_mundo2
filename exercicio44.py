5f4f28f3f4dd3ee606718883dbd569e8f2b11446
Luis Heitor Santana <103472722+hetoi@users.noreply.github.com>
2025-03-13 14:29:20 -0300

    Update exercicio44.py

--- exercicio44.py
+++ exercicio44.py
@@ -7,26 +7,30 @@
 # – 3x ou mais no cartão: 20% de juros
 
 preco = float(input("Qual o valor da compra? R$"))
-print("FORMAS DE PAGAMENTO")
-print("[1] à vista dinheiro/cheque")
-print("[2] à vista no cartão")
-print("[3] 2x no cartão")
-print("[4] 3x ou mais no cartão")
+print("""FORMAS DE PAGAMENTO
+[1] à vista dinheiro/cheque
+[2] à vista no cartão
+[3] 2x no cartão
+[4] 3x ou mais no cartão
+""")
 pagamento = int(input("Qual a opção? "))
 
 if pagamento == 1:
-    preco = preco*0.9
-    print("O valor da compra com 10% de desconto será de RS{:.2f}".format(preco))
+    total = preco*0.9
+    print("O valor da compra com 10% de desconto será de R${:.2f}".format(total))
 elif pagamento == 2:
-    preco = preco * 0.95
-    print("O valor da compra com 5% de desconto será de R${:.2f}".format(preco))
+    total = preco * 0.95
+    print("O valor da compra com 5% de desconto será de R${:.2f}".format(total))
 elif pagamento == 3:
+    total = preco
     parcela = (preco/2)
-    print("O valor total da compra será {:.2f}".format(preco))
+    print("O valor total da compra será R${:.2f}".format(total))
     print("Cada parcela ficará no valor de R${:.2f}".format(parcela))
 elif pagamento == 4:
     qtd_parcelas = int(input("Em quantas parcelas? "))
-    preco_final = preco*1.2
-    parcela = preco_final/qtd_parcelas
-    print("O valor total da compra com 20% de juros será R${:.2f}".format(preco_final))
+    total = preco*1.2
+    parcela = total/qtd_parcelas
+    print("O valor total da compra com 20% de juros será R${:.2f}".format(total))
     print("Em {} parcelas, cada uma ficará no valor de R${:.2f}.".format(qtd_parcelas, parcela))
+else:
+    print("OPÇÃO INVÁLIDA > TENTE NOVAMENTE!")

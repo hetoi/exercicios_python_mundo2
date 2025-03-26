7424ba8768bde1c567c627ab8356e2517428fe43
Luis Heitor Santana <103472722+hetoi@users.noreply.github.com>
2025-03-26 13:10:01 -0300

    Resolução com mais detalhes
    Foi testado para validar código e deu certo.
    Optei por colocar os comentários em algumas linhas pois facilita a compreensão

--- exercicio45.py
+++ exercicio45.py
@@ -3,6 +3,7 @@
 import random
 from time import sleep
 
+# Mensagem inicial
 print("""
 SUAS OPÇÕES: 
 [ 1 ] PEDRA
@@ -10,31 +11,41 @@ SUAS OPÇÕES:
 [ 3 ] TESOURA
 """)
 
+# Opção que o jogador escolheu
 escolha_jogador = int(input("Qual a sua jogada? "))
-escolha_programa = random.randint(1,3)
-if escolha_jogador <= 3:
+
+if escolha_jogador >= 1 and escolha_jogador <= 3:
+    escolha_programa = random.randint(1,3)
+    
     print("JO")
     sleep(1)
     print("KEN")
     sleep(1)
     print("PO!!!")
     
+    # Todas as coombinações possíveis
+    # Jogador tem três opções de escolha, em cada uma ele pode ganhar, perder, ou empatar
+
+    # Caso empate
     if escolha_jogador == escolha_programa :
         mensagem = "O jogo EMPATOU!"
-    elif escolha_jogador == 1 and escolha_programa == 2:
-        mensagem = "O jogador PERDEU"
-    elif escolha_jogador == 1 and escolha_programa == 3:
+
+    # Caso ganhe
+    elif (escolha_jogador == 1 and escolha_programa == 3) or \
+         (escolha_jogador == 2 and escolha_programa == 1) or \
+         (escolha_jogador == 3 and escolha_programa == 2):
         mensagem = "O jogador GANHOU"
-    elif escolha_jogador == 2 and escolha_programa == 3: 
+    # Caso perca
+    else: 
         mensagem = "O jogador PERDEU" 
     
-    # É preciso .format dos prints seguintes 
-    # Quebrar a linha da mensagem 
-    print("COMPUTADOR JOGOU {}".format(escolha_jogador))
-    print("JOGADOR JOGOU {}".format(escolha_programa))
-    print(str(mensagem))
+    # Mensagem retornano resultado
+    print("O JOGADOR JOGOU {}".format(escolha_jogador))
+    print("COMPUTADOR JOGOU {}".format(escolha_programa))
+    print(mensagem)
 
 else:
+    # Casos em que a não se escolhe entre 1 e 3
     print("OPÇÃO INVÁLIDA!")
  
 

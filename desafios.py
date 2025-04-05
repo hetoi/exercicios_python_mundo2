c2bda9cc1bfc97fd6742007dea04768045d268a0
Luis Heitor Santana <103472722+hetoi@users.noreply.github.com>
2025-04-05 14:04:59 -0300

    Update desafios.py

--- desafios.py
+++ desafios.py
@@ -1,5 +1,9 @@
 # Resolvendo desafios que pedi ao DeepSeek
 
+######################
+######### 1 ##########
+######################
+
 # Crie um programa que pede um número inteiro N ao usuário 
 # e calcula a soma de todos os números de 1 até N.
 
@@ -11,12 +15,17 @@ for i in range(1, numero+1):
 print(saida)
 
 
+######################
+######### 2 ##########
+######################
+
 # Faça um programa que conta quantas vogais (a, e, i, o, u) 
 # existem em uma palavra digitada pelo usuário.
 palavra = str(input("Digite uma palavra: "))
 saida2 = 0
 
 for i in palavra: 
+    # poderia ser if i == "a" or i == "e" or i == "i" or i == "o" or i == "u":
     if i == "a":
         saida2 += 1
     elif i == "e":
@@ -41,7 +50,19 @@ for letra in palavra:
 
 print(contador)
 
-
-# solução paralela para o caso i == "a" or i == "e" or ...
 # a primeira tentativa estava dando errado pois eu tentei i == "a" or "e" or "i"...
 # observe que entre OR/AND fica a condição T ou F, "'e' or 'i'" não está apto pra ser T ou F
+
+######################
+######### 3 ##########
+######################
+
+
+# Crie um programa que imprime uma pirâmide de números como no exemplo abaixo para um número N digitado:
+
+n = int(input("Digite um número: "))
+
+for linha in range(1, n+1):
+    espaco = " " * (n-linha)
+    numeros = str(linha) * linha
+    print(espaco+numeros)
